17b4df053024c0501ea8a00072a38d9cec0ce3da
Mark Levine <mark.levinejr@gmail.com>
2018-11-04 22:13:35 -0800

    Add files via upload

--- Unit 14 - Assignment - JavaScript and DOM Manipulation/static/js/app.js
+++ Unit 14 - Assignment - JavaScript and DOM Manipulation/static/js/app.js
@@ -1,28 +1,16 @@
 // from data.js
 var tableData = data;
-
-
-
-
-
-
-// var clearconents = d3.select('#clear-btn');
-// clearcontents.on('click', function(){
-//     d3.event.preventDefault();
-// })
-// clearconents.on('click', function(event){
-//     d3.event.preventDefault();
-// })
-
-
-
 var submit = d3.select('#filter-btn');
 
-
+//create out function that reacts when user clicks the submit button
 submit.on('click', function(){
 
+    //we are preventing refresh and also removing any 
+    //previous search results or No results paragraphs
     d3.event.preventDefault();
-    
+    d3.selectAll('th').remove()
+    d3.selectAll('td').remove()
+    d3.selectAll('p').remove()
     
     var inputElement = d3.select('#datetime');
     
@@ -34,9 +22,8 @@ submit.on('click', function(){
     var filteredData = tableData.filter(date => date.datetime === inputValue);
 
     console.log(filteredData);
-    function clear(data){
-        
-    }
+    //in this function, data and columns are recognized
+    //by d3/js 
     function tabulate(data, columns){
         
         var table = d3.select("#ufo-table").append('table'),
@@ -71,12 +58,20 @@ submit.on('click', function(){
 
 
     }//call the tabulate function as pass our filtered data as well as the columns we want
-
+    //if filtered data is an empty list, tell the user no results
+    //and print to console no results. other wise, call our
+    //tabulate data function with the following columns
+    if(filteredData.length === 0){console.log('noresults');
+    d3.event.preventDefault();
+    var table = d3.select("#ufo-table").append('table'),
+    thead = table.append('thead')
+    thead.append('p').text('No Results Try Again') }
+    else{
 
     var data_ = tabulate(filteredData,
     ['datetime','city','state','country','shape','durationMinutes','comments']);
    
-
+    }
     
 });
 
